ba6f658d29e44e73f98ad2f48640f73137e2669f
Chidi <chidex5@gmail.com>
2022-02-02 08:24:39 +0100

    fix tailwind template paths config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,4 @@
 module.exports = {
-	mode: 'jit',
-	// These paths are just examples, customize them to match your project structure
-	purge: ['./public/**/*.html', './src/**/*.{js,jsx,ts,tsx,vue}'],
 	content: [
 		'./pages/**/*.{js,ts,jsx,tsx}',
 		'./components/**/*.{js,ts,jsx,tsx}',
