df72fca55ee92bd103f7f3df12f4a3943fa2daf4
Chidi <chidex5@gmail.com>
2023-02-03 03:25:26 +0100

    Fix react hot toast setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,13 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import Layout from '../components/Layout';
 import AppContextProvider from '../state/context/appContext';
+import { Toaster } from 'react-hot-toast';
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
 		<AppContextProvider>
 			<Layout>
+				<Toaster />
 				<Component {...pageProps} />
 			</Layout>
 		</AppContextProvider>
