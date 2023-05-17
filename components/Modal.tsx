df50fce6c7c2275c61eee162c54e2dd134e7ad2d
Chidi <chidex5@gmail.com>
2023-05-17 20:40:26 +0100

    Improve modal styling

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -17,7 +17,7 @@ const Modal = ({ isOpen, handleClick, title, children }: Props) => {
 			<div className="relative w-auto pointer-events-none">
 				<div className="border-none shadow-lg relative flex flex-col w-full pointer-events-auto bg-white bg-clip-padding rounded-md outline-none text-current">
 					<div className="flex flex-shrink-0 items-center justify-between p-4 border-b border-gray-200 rounded-t-md">
-						<h5 className="text-xl font-medium leading-normal text-gray-800">
+						<h5 className="text-base font-medium leading-normal text-gray-800">
 							{title}
 						</h5>
 						<div className="cursor-pointer" onClick={handleClick}>
